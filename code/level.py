c9a3199858fc9a028019403443ff4098e3de0b83
Kyoko <47301396+catgirlchris@users.noreply.github.com>
2022-12-27 18:44:01 +0100

    offset and camera implementation

--- code/level.py
+++ code/level.py
@@ -13,7 +13,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstables_sprites = pygame.sprite.Group()
 
         # sprite set up
@@ -27,10 +27,31 @@ class Level:
                 if col == 'x':
                     Tile((x,y), [self.visible_sprites, self.obstables_sprites])
                 if col == 'p':
-                    self.player = Player((x,y), [self.visible_sprites], self.obstables_sprites)
+                    self.player : Player = Player((x,y), [self.visible_sprites], self.obstables_sprites)
 
     def run(self):
         # update an draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        
+        # debug info
         debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player : Player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset # no longer a Vector, but pygame still likes Tuple
+            self.display_surface.blit(sprite.image, offset_pos)
