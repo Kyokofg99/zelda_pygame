e7e3fcc5b79a855471b26e35709d1a5a26dee344
Kyoko <47301396+catgirlchris@users.noreply.github.com>
2022-12-27 10:56:40 +0100

    player movement structure

--- code/level.py
+++ code/level.py
@@ -4,6 +4,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -26,8 +27,10 @@ class Level:
                 if col == 'x':
                     Tile((x,y), [self.visible_sprites])
                 if col == 'p':
-                    Player((x,y), [self.visible_sprites])
+                    self.player = Player((x,y), [self.visible_sprites])
 
     def run(self):
         # update an draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
